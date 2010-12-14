f8535efcc82459d4ae828a2aca23b75da966db39
Martin Thielecke <codebasehq@versionzwei.com>
2010-12-14 18:55:32 +0100

    - added ui for new keys

--- templates/js/functions.js
+++ templates/js/functions.js
@@ -20,7 +20,62 @@ function CloudKeys() {
     var that = this;
     $.get('/templates/list_keys.html', function(data) {
       $('#content').html(data);
+      $('#button_create_key').click(function() {
+        $.get('/templates/create_key.html', function(data) {
+          var message = $('<div id="dialog-modal" title="Create Key">'+ data +'</div>');
+          $('#content').append(message);
+          $("#dialog:ui-dialog").dialog("destroy");
+          $("#dialog-modal").dialog({
+            height: 260,
+            modal: true,
+            width: 360
+          });
+          $('#create_save').click(function() {
+            that.create_key();
+          });
+        });
+      });
     });
+  }
+
+  this.create_key = function() {
+    var that = this;
+    var data = { };
+    var errors = 0;
+    $('.missing_field').remove();
+
+    if($('#create_title').val() == '') {
+      $('<span class="missing_field">!</span>').insertAfter($('#create_title'));
+      errors = errors + 1;
+    }
+
+    if($('#create_username').val() == '') {
+      $('<span class="missing_field">!</span>').insertAfter($('#create_username'));
+      errors = errors + 1;
+    }
+
+    if($('#create_password').val() == '') {
+      $('<span class="missing_field">!</span>').insertAfter($('#create_password'));
+      errors = errors + 1;
+    }
+
+    if($('#create_password_repeat').val() == '' || $('#create_password_repeat').val() != $('#create_password').val()) {
+      $('<span class="missing_field">!</span>').insertAfter($('#create_password_repeat'));
+      errors = errors + 1;
+    }
+
+    if($('#create_url').val() == '') {
+      $('<span class="missing_field">!</span>').insertAfter($('#create_url'));
+      errors = errors + 1;
+    }
+
+    if(errors == 0) {
+      data.title = $('#create_title').val();
+      data.username = $('#create_username').val();
+      data.password = $('#create_password').val();
+      data.url = $('#create_url').val();
+      console.log(data);
+    }
   }
 
   this.decrypt_data = function() {
