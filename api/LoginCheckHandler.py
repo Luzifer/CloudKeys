9ec199438a60bf704d835af97fd459d657d742b8
Knut Ahlers <knut@ahlers.me>
2010-12-14 16:15:15 +0100

    Added logincheck

--- api/LoginCheckHandler.py
+++ api/LoginCheckHandler.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from google.appengine.ext import webapp
+from google.appengine.api import users
+
+from django.utils import simplejson as json
 
 class LoginCheckHandler(webapp.RequestHandler):
   """Lists all texts of type post to display for example on start page"""
   def get(self):
-    self.response.out.write('{}')
+    user = users.get_current_user()
+    result = {}
+    if user == None:
+      result['isLoggedIn'] = False
+      result['loginURL'] = users.create_login_url('/')
+    else:
+      result['isLoggedIn'] = True
+    
+    self.response.out.write(json.dumps(result))
