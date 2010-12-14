2f9ee72685cc67d08873f08642603fc93287df65
Knut Ahlers <knut@ahlers.me>
2010-12-14 17:03:58 +0100

    Added model for password storage

---
+++ models/StoredKey.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from google.appengine.ext import db
+
+class StoredKey(db.Model):
+  title = db.StringProperty(required=True, verbose_name="Title")
+  password = db.StringProperty(required=True, verbose_name="Password")
+  username = db.StringProperty(required=True, verbose_name="Username")
+  url = db.StringProperty(required=False, verbose_name="URL")
+  note = db.TextProperty(verbose_name="Note")
+  lastChange = db.DateTimeProperty(verbose_name="Last Change", auto_now=True)
+  user = db.UserProperty(verbose_name="Owner", auto_current_user_add=True)
+  category = db.StringProperty(required=False, verbose_name="Category")
+  
+  def to_d(self):
+    result = {}
+    result['title'] = self.title
+    result['password'] = self.password
+    result['username'] = self.username
+    result['url'] = self.url
+    result['note'] = self.note
+    result['category'] = self.category
+    return result
