1e168e2d153270d7479242af17d988da8f7b0512
Martin Thielecke <codebasehq@versionzwei.com>
2010-12-14 20:54:58 +0100

    Yeah! We have decrypted passwords now

--- templates/js/functions.js
+++ templates/js/functions.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
 function CloudKeys() {
   this.password = '';
-  this.data = { };
+  this.data = [];
 
   this.show_list = function() {
     var that = this;
@@ -28,6 +28,7 @@ function CloudKeys() {
           $("#dialog-modal").dialog({
             height: 260,
             modal: true,
+            resizable: false,
             width: 360
           });
           $('#create_save').click(function() {
@@ -40,61 +41,75 @@ function CloudKeys() {
 
   this.create_key = function() {
     var that = this;
-    var data = { };
+    var data = {};
     var errors = 0;
     $('.missing_field').remove();
+    var errorMessage = '<span class="missing_field">!</span>';
 
     if($('#create_title').val() == '') {
-      $('<span class="missing_field">!</span>').insertAfter($('#create_title'));
+      $(errorMessage).insertAfter($('#create_title'));
       errors = errors + 1;
     }
 
     if($('#create_username').val() == '') {
-      $('<span class="missing_field">!</span>').insertAfter($('#create_username'));
+      $(errorMessage).insertAfter($('#create_username'));
       errors = errors + 1;
     }
 
     if($('#create_password').val() == '') {
-      $('<span class="missing_field">!</span>').insertAfter($('#create_password'));
+      $(errorMessage).insertAfter($('#create_password'));
       errors = errors + 1;
     }
 
     if($('#create_password_repeat').val() == '' || $('#create_password_repeat').val() != $('#create_password').val()) {
-      $('<span class="missing_field">!</span>').insertAfter($('#create_password_repeat'));
+      $(errorMessage).insertAfter($('#create_password_repeat'));
       errors = errors + 1;
     }
 
     if($('#create_url').val() == '') {
-      $('<span class="missing_field">!</span>').insertAfter($('#create_url'));
+      $(errorMessage).insertAfter($('#create_url'));
       errors = errors + 1;
     }
 
     if(errors == 0) {
-      data.title = $('#create_title').val();
-      data.username = $('#create_username').val();
-      data.password = $('#create_password').val();
-      data.url = $('#create_url').val();
-      console.log(data);
+      data.title = Crypto.AES.encrypt($('#create_title').val(), this.password);
+      data.username = Crypto.AES.encrypt($('#create_username').val(), this.password);
+      data.password = Crypto.AES.encrypt($('#create_password').val(), this.password);
+      data.url = Crypto.AES.encrypt($('#create_url').val(), this.password);
+      $.post('/api/saveKey', data, function(data) {
+        alert(data);
+      }, 'json');
     }
   }
 
   this.decrypt_data = function() {
     var that = this;
-    $.get('/api/getKeys', function(data) {
-      /*
-      try {
-        var plain = Crypto.AES.decrypt(crypted, that.password);
-      } catch(ex) {
-        var message = $('<div id="dialog-modal" title="Error"><p>Failed to decrypt your keys. Please check your password!</p></div>');
-        $('#content').append(message);
-        $("#dialog:ui-dialog").dialog("destroy");
-        $("#dialog-modal").dialog({
-          height: 140,
-          modal: true
-        });
-        that.show_password_field();
-      } */
-      that.show_list();
+    $.getJSON('/api/getKeys', function(data) {
+      if(data.status == true) {
+        try {
+          $.each(data.passwords, function(index, value) {
+            console.log(index);
+            console.log(value);
+            that.data.push({
+                key: value.key
+              , title: Crypto.AES.decrypt(value.title, that.password)
+              , username: Crypto.AES.decrypt(value.username, that.password)
+              , password: Crypto.AES.decrypt(value.password, that.password)
+              , url: Crypto.AES.decrypt(value.url, that.password)
+            });
+          });
+          that.show_list();
+        } catch(ex) {
+          that.show_password_field();
+          var message = $('<div id="dialog-modal" title="Error"><p>Failed to decrypt your keys. Please check your password!</p></div>');
+          $('#content').append(message);
+          $("#dialog:ui-dialog").dialog("destroy");
+          $("#dialog-modal").dialog({
+            height: 140,
+            modal: true
+          });
+        }
+      }
     });
   }
 
